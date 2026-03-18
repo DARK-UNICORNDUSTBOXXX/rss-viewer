df26e57a5f3d2a9225186ab5d4009fdb60eedd55
DARK-UNICORNDUSTBOXXX <rislim199999@gmail.com>
2026-03-19 03:48:02 +0900

    Update generate.py

--- generate.py
+++ generate.py
@@ -1,63 +1,112 @@
 import feedparser
+import json
+import random
+import html
 import requests
 from bs4 import BeautifulSoup
-import html
 
-rss_list = {
-    "fig速": "https://figsoku.net/blog-feed.xml",
-    "電撃ホビー": "https://hobby.dengeki.com/feed/",
-    "ホビーサーチ": "https://www.1999.co.jp/blog/feed"
-}
+# ------------------------
+# RSS読み込み
+# ------------------------
 
-def get_thumbnail(url):
-    try:
-        r = requests.get(url, timeout=10)
-        soup = BeautifulSoup(r.text, "html.parser")
+with open("rss.json", encoding="utf-8") as f:
+    sites = json.load(f)
 
-        og = soup.find("meta", property="og:image")
-        if og:
-            return og["content"]
+# ------------------------
+# メルカリ画像取得
+# ------------------------
 
-        img = soup.find("img")
-        if img:
-            return img["src"]
+SEARCH_URL = "https://jp.mercari.com/search?keyword=初音ミク フィギュア"
+AFF_LINK = "https://jp.mercari.com/search?afid=2445940742&keyword=%E5%88%9D%E9%9F%B3%E3%83%9F%E3%82%AF+%E3%83%95%E3%82%A3%E3%82%AE%E3%83%A5%E3%82%A2"
 
-    except:
-        pass
+imgs = []
 
-    return ""
+try:
 
-html_output = ""
+    headers = {
+        "User-Agent": "Mozilla/5.0"
+    }
 
-for site, rss in rss_list.items():
+    r = requests.get(SEARCH_URL, headers=headers)
+    soup = BeautifulSoup(r.text, "html.parser")
 
-    html_output += f'<div class="rss-site">\n'
-    html_output += f'<div class="rss-site-title">{site}</div>\n'
+    for img in soup.find_all("img"):
 
-    feed = feedparser.parse(rss)
+        src = img.get("src")
 
-    for entry in feed.entries[:15]:
+        if src and "mercari" in src and "item" in src:
+            imgs.append(src)
+
+except:
+    pass
+
+# 6枚ランダム
+ads = random.sample(imgs, min(6, len(imgs)))
+
+# ------------------------
+# HTML生成
+# ------------------------
+
+html_out = """
+<!DOCTYPE html>
+<html>
+<head>
+<meta charset="utf-8">
+<title>RSSまとめ</title>
+<link rel="stylesheet" href="style.css">
+</head>
+
+<body>
+
+<h1>RSSまとめ</h1>
+
+<div class="container">
+
+<div class="left">
+"""
+
+# ------------------------
+# 広告
+# ------------------------
+
+html_out += f'<a href="{AFF_LINK}" target="_blank">'
+
+for img in ads:
+    html_out += f'<img src="{img}">'
+
+html_out += "</a></div>"
+
+# ------------------------
+# RSSカラム
+# ------------------------
+
+html_out += '<div class="main">'
+
+for site in sites:
+
+    name = site["name"]
+    url = site["url"]
+    top = site["top"]
+
+    feed = feedparser.parse(url)
+
+    html_out += f"<h3>{html.escape(name)}</h3>"
+    html_out += "<ul>"
+
+    for entry in feed.entries[:top]:
 
         title = html.escape(entry.title)
         link = entry.link
 
-        thumb = get_thumbnail(link)
+        html_out += f'<li><a href="{link}" target="_blank">{title}</a></li>'
 
-        html_output += '<div class="rss-item">\n'
+    html_out += "</ul>"
 
-        if thumb:
-            html_output += f'<a href="{link}" target="_blank"><img class="rss-thumb" src="{thumb}"></a>\n'
+html_out += "</div></div></body></html>"
 
-        html_output += f'''
-        <div class="rss-title">
-        <a href="{link}" target="_blank">{title}</a>
-        </div>
-        '''
+# ------------------------
+# 出力
+# ------------------------
 
-        html_output += '</div>\n'
-
-    html_output += "</div>\n"
-
-
-with open("rss.html", "w", encoding="utf-8") as f:
-    f.write(html_output)
+with open("index.html", "w", encoding="utf-8") as f:
+    f.write(html_out)
