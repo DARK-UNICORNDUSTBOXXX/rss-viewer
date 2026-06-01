33ed3aa76a3449ee96e457d2da59c842f1fd1238
DARK-UNICORNDUSTBOXXX <rislim199999@gmail.com>
2026-06-02 02:01:01 +0900

    Update generate.py

--- generate.py
+++ generate.py
@@ -6,7 +6,7 @@ from bs4 import BeautifulSoup
 from datetime import datetime, timedelta
 
 JST_OFFSET = 9
-
+feedparser.USER_AGENT = "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/120.0.0.0 Safari/537.36"
 # RSSサイト読み込み
 with open("sites.json", "r", encoding="utf-8") as f:
     sites = json.load(f)
